99df7e83ee94d092a76322b3951df4bc93c4e253
Kevin200500 <201.english.rivas@gmail.com>
2020-09-23 12:30:20 -0600

    RESTFUL funcionando

---
+++ REST/REST/Controllers/ContactController.cs
@@ -0,0 +1,40 @@
+﻿using REST.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace REST.Controllers
+{
+    public class ContactController : ApiController
+    {
+        [HttpGet]
+        public Person[] Get()
+        {
+            return new Person[]
+            {
+                
+                new Person
+                {
+                    Id= 1,
+                    Name="Kevin",
+                    LastName="Rivas",
+                    NickName="Kevin200500",
+                    Email="201.english.rivas@gmail.com",
+                    password="12345678"
+                },
+                new Person
+                {
+                    Id= 2,
+                    Name="Roberto",
+                    LastName="Aguilar",
+                    NickName="Roberto20-05-00",
+                    Email="bmwgtr3.200500@hotmail.com",
+                    password="12345678"
+                }
+            };
+        }
+    }
+}
